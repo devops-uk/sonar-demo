706ea29bbe79da1b43e45e9c836281c78556eb77
devops-uk <61527367+devops-uk@users.noreply.github.com>
2026-02-15 21:36:45 -0500

    Refactor AppTest and add divide by zero test
    Refactor test assertions for consistency and add test for divide by zero.

--- src/test/java/com/lab/AppTest.java
+++ src/test/java/com/lab/AppTest.java
@@ -1,21 +1,25 @@
 package com.lab;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
     @Test
     public void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2,3));
+        assertEquals(5, app.add(2, 3));
     }
 
     @Test
     public void testDivide() {
         App app = new App();
-        assertEquals(2, app.divide(4,2));
-        assertEquals(0, app.divide(4,0));
+        assertEquals(2, app.divide(4, 2));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        App app = new App();
+        assertEquals(0, app.divide(4, 0));
     }
 }
-
